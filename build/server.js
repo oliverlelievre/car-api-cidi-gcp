9bc79ae763284f1320359685e2a0ded77edc7880
oliverlelievre <oliverl@missionreadyhq.com>
2023-05-22 14:56:08 +1200

    remade build folder

--- build/server.js
+++ build/server.js
@@ -3,12 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const carValueRouter_1 = __importDefault(require("./routes/carValueRouter"));
-const app = (0, express_1.default)();
-const port = 3000;
-app.use(express_1.default.json());
-app.use('/car-value', carValueRouter_1.default);
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const dotenv_1 = __importDefault(require("dotenv"));
+const app_1 = __importDefault(require("./app"));
+dotenv_1.default.config();
+const PORT = process.env.PORT;
+app_1.default.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
 });
